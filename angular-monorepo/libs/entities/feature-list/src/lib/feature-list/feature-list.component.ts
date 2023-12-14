fff3cd25935328bcccd048eade5cde259cd22492
Olga Budova <olgabudova55@gmail.com>
2023-12-14 13:28:50 +0100

    feat(entities-list): implement table

--- angular-monorepo/libs/entities/feature-list/src/lib/feature-list/feature-list.component.ts
+++ angular-monorepo/libs/entities/feature-list/src/lib/feature-list/feature-list.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { EntityServiceInterface } from '@angular-monorepo/entities/data-repository';
+import { RxState } from '@rx-angular/state';
+import { GetEntityListParams } from '../../../../data-repository/src/lib/model/model';
 
+interface State extends GetEntityListParams{
+
+}
 @Component({
   selector: 'angular-monorepo-feature-list',
   templateUrl: './feature-list.component.html',
   styleUrls: ['./feature-list.component.scss'],
+  providers: [RxState],
 })
-export class FeatureListComponent {}
+export class FeatureListComponent {
+
+  entities$ = this.entityService.getEntityList(this._state.get())
+
+  constructor(@Inject('ENTITY_SERVICE') private entityService: EntityServiceInterface,
+              private _state: RxState<State>) { }
+}
