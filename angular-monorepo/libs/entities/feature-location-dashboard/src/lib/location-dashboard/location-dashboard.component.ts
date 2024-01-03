55891aa07d76de08dac1bb3bf5a8c4ba9bdae349
Olga Budova <olgabudova55@gmail.com>
2024-01-03 16:55:59 +0100

    feat(location-dashboard): run chart initialization outside of ang

--- angular-monorepo/libs/entities/feature-location-dashboard/src/lib/location-dashboard/location-dashboard.component.ts
+++ angular-monorepo/libs/entities/feature-location-dashboard/src/lib/location-dashboard/location-dashboard.component.ts
@@ -1,7 +1,16 @@
-import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  ElementRef,
+  inject,
+  Inject,
+  NgZone,
+  ViewChild,
+} from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { EntityServiceInterface } from '@angular-monorepo/entities/data-repository';
 import { EmployeeVisits } from '@angular-monorepo/entities/data-repository';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'angular-monorepo-location-dashboard',
@@ -12,80 +21,98 @@ export class LocationDashboardComponent {
   @ViewChild('chartContainer') chartContainer!: ElementRef;
   @ViewChild('chartEmployee') chartEmployee!: ElementRef;
   loading = true;
+  destroyRef = inject(DestroyRef);
 
-  constructor(@Inject('ENTITY_SERVICE') private entityService: EntityServiceInterface) {
-    this.entityService.getLocationStats().subscribe((val) => {
-      this.drawLocationVisits(val.lastWeekLocationOccupancy);
-      this.drawEmployeeVisitsChart(val.lastWeekEmployeesVisits);
-    })
+  constructor(
+    @Inject('ENTITY_SERVICE') private entityService: EntityServiceInterface,
+    private zone: NgZone
+  ) {
+    this.entityService
+      .getLocationStats()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((val) => {
+        this.drawLocationVisits(val.lastWeekLocationOccupancy);
+        this.drawEmployeeVisitsChart(val.lastWeekEmployeesVisits);
+      });
   }
 
   drawEmployeeVisitsChart(employeeVisits: EmployeeVisits[]) {
-    Highcharts.chart(this.chartEmployee.nativeElement, {
-      chart: {
-        type: 'column'
-      },
-      title: {
-        text: 'Last week visits per employee',
-        align: 'left'
-      },
-      xAxis: {
-        categories: employeeVisits.map((employee)=> employee.name),
-        crosshair: true,
-        accessibility: {
-          description: 'Last week location visits per employee'
-        }
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Amount of Visits'
-        }
-      },
-      plotOptions: {
-        column: {
-          pointPadding: 0.2,
-          borderWidth: 0
-        }
-      },
-      series: [
-        {
+    this.zone.runOutsideAngular(() => {
+      Highcharts.chart(this.chartEmployee.nativeElement, {
+        chart: {
           type: 'column',
-          name: 'Employee visited (times)',
-          data: employeeVisits.map(employee => employee.visits)
         },
-      ]
+        title: {
+          text: 'Last week visits per employee',
+          align: 'left',
+        },
+        xAxis: {
+          categories: employeeVisits.map((employee) => employee.name),
+          crosshair: true,
+          accessibility: {
+            description: 'Last week location visits per employee',
+          },
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Amount of Visits',
+          },
+        },
+        plotOptions: {
+          column: {
+            pointPadding: 0.2,
+            borderWidth: 0,
+          },
+        },
+        series: [
+          {
+            type: 'column',
+            name: 'Employee visited (times)',
+            data: employeeVisits.map((employee) => employee.visits),
+          },
+        ],
+      });
     });
 
-    this.loading = false
+    this.loading = false;
   }
 
   drawLocationVisits(locationVisits: number[]) {
-    // Create the chart
-    Highcharts.chart(this.chartContainer.nativeElement, {
-      chart: {
-        type: 'line'
-      },
-      title: {
-        text: 'Weekday Data - Location Occupation'
-      },
-      xAxis: {
-        categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-      },
-      yAxis: {
-        title: {
-          text: 'Amount of Visits'
+    this.zone.runOutsideAngular(() => {
+      Highcharts.chart(this.chartContainer.nativeElement, {
+        chart: {
+          type: 'line',
         },
-      },
-      series: [{
-        name: 'Location Prokocim Szpital',
-        type: 'line',
-        data: locationVisits
-      }]
+        title: {
+          text: 'Weekday Data - Location Occupation',
+        },
+        xAxis: {
+          categories: [
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday',
+          ],
+        },
+        yAxis: {
+          title: {
+            text: 'Amount of Visits',
+          },
+        },
+        series: [
+          {
+            name: 'Location Prokocim Szpital',
+            type: 'line',
+            data: locationVisits,
+          },
+        ],
+      });
     });
 
-    this.loading = false
-
+    this.loading = false;
   }
-
 }
